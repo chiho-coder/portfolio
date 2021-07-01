a53f15d22b19139d41bd673a6a8f6db0bc0d647c
chihozoo@yahoo.co.jp <chihozoo@yahoo.co.jp>
2021-07-01 08:19:28 +0000

    7-1

--- public/js/app.js
+++ public/js/app.js
@@ -49810,6 +49810,17 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/front.scss":
+/*!***********************************!*\
+  !*** ./resources/sass/front.scss ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/sass/post.scss":
 /*!**********************************!*\
   !*** ./resources/sass/post.scss ***!
@@ -49822,15 +49833,16 @@ __webpack_require__.r(__webpack_exports__);
 /***/ }),
 
 /***/ 0:
-/*!****************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/post.scss ***!
-  \****************************************************************************************/
+/*!********************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/post.scss ./resources/sass/front.scss ***!
+  \********************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /home/ec2-user/environment/mycmn/portfolio/resources/js/app.js */"./resources/js/app.js");
 __webpack_require__(/*! /home/ec2-user/environment/mycmn/portfolio/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /home/ec2-user/environment/mycmn/portfolio/resources/sass/post.scss */"./resources/sass/post.scss");
+__webpack_require__(/*! /home/ec2-user/environment/mycmn/portfolio/resources/sass/post.scss */"./resources/sass/post.scss");
+module.exports = __webpack_require__(/*! /home/ec2-user/environment/mycmn/portfolio/resources/sass/front.scss */"./resources/sass/front.scss");
 
 
 /***/ })
